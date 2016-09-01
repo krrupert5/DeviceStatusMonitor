c4ae726b2df694123f1c7cf108e5531328104452
Kyle Rupert <krrupert5@gmail.com>
2016-09-01 07:55:32 -0400

    Added battery capability, changed the duration to days, split the preferences into multiple pages

--- smartapps/krrupert5/device-status-monitor.src/device-status-monitor.groovy
+++ smartapps/krrupert5/device-status-monitor.src/device-status-monitor.groovy
@@ -3,12 +3,13 @@
  *
  *  Copyright 2016 Kyle Rupert
  *
- *  Version 1.0.1	31 Aug 2016
+ *  Version 1.0.2	1 Sep 2016
  * 
  *  Version History
  * 
  *  1.0.0 	30 Aug 2016		Initial Release
  *	1.0.1	31 Aug 2016		Fixed an issue with the threshold calculation
+ *  1.0.2 	1  Sep 2016		Added battery capability, changed the duration to days, split the preferences into multiple pages
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -20,6 +21,8 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+ 
+ 
 definition(
     name: "Device Status Monitor",
     namespace: "krrupert5",
@@ -32,22 +35,36 @@ definition(
 
 
 preferences {
-	section("Temperatures to monitor") {
-    	input "temperatures", "capability.temperatureMeasurement", required: false, title: "Temperatures: ", multiple: true
+	page(name: "selectDevices", title: "Select Devices to Monitor", nextPage: "selectPollSettings", uninstall: true) {
+        section("Temperatures to monitor") {
+            input "temperatures", "capability.temperatureMeasurement", required: false, title: "Temperatures: ", multiple: true
+        }
+        
+        section("Warning") {
+        	paragraph "Battery statuses are not updated by most devices as often as temperatures, if using battery to monitor a device then make sure the threshold is larger (~ 7 days)"
+        }
+        
+        section("Battery Status to monitor") {
+        	input "batteries", "capability.battery", required: false, title: "Batteries: ", multiple: true
+        }
+
+        section("Contacts to monitor") {
+            input "contacts", "capability.contactSensor", required: false, title: "Contacts: ", multiple: true
+        }   
     }
     
-    section("Contacts to monitor") {
-    	input "contacts", "capability.contactSensor", required: false, title: "Contacts: ", multiple: true
+    page(name: "selectPollSettings", title: "Select the Polling Settings", nextPage: "setNotifications") {
+ 	    section("Check Every") {
+            input "days", "enum", required: true, title: "Days: ", options: [1, 2, 3, 4, 5, 6, 7]
+        }   
     }
     
-    section("Check Every") {
-    	input "hours", "enum", required: true, title: "Hours: ", options: [24, 48, 72, 96]
-    }
-    
-    section("Notifications") {
-    	input("recipients", "contact", title: "Send notifications to", required: false) {
-        	input "sendPushMessage", "enum", title: "Send a Push Notification?", options: ["Yes", "No"], required: false
-            input "phone", "phone", title: "Send a Text Message?", required: false
+    page(name: "setNotifications", title: "Set the Notification Settings", install: true) {
+        section("Notifications") {
+            input("recipients", "contact", title: "Send notifications to", required: false) {            
+                input "sendPushMessage", "enum", title: "Send a Push Notification?", options: ["Yes", "No"], required: false
+                input "phone", "phone", title: "Send a Text Message?", required: false
+            }            
         }
     }
 }
@@ -67,62 +84,61 @@ def updated() {
 }
 
 def initialize() {   
-	// actual hours 
-    switch(hours) {
-    	case "24":
-        	schedule("0 0 0 1/1 * ? *", handlerMethod)
-            break
-        case "48":
-        	schedule("0 0 0 1/2 * ? *", handlerMethod)
-            break
-        case "72":
-        	schedule("0 0 0 1/3 * ? *", handlerMethod)
-            break
-        case "96":
-        	schedule("0 0 0 1/4 * ? *", handlerMethod)
-            break
-    }
+    schedule("0 0 0 1/$days * ? *", handlerMethod)
 }
 
 def handlerMethod() {  
-    def hourInt = 24
+    def daysInt = 1
 
-    switch(hours) {
-    	case "24":
-        	hourInt = 24
+    switch(days) {
+    	case "1":
+        	daysInt = 1
             break
-        case "48":
-        	hourInt = 48
+        case "2":
+        	daysInt = 2
             break
-        case "72":
-        	hourInt = 72
+        case "3":
+        	daysInt = 3
+        	break
+        case "4":
+        	daysInt = 4
             break
-        case "96":
-        	hourInt = 96
+        case "5":
+        	daysInt = 5
+            break
+        case "6":
+        	daysInt = 6
+            break
+        case "7":
+        	daysInt = 7
             break
     }
         
     temperatures.each { temp ->
-    	checkDevice(temp, "temperature", hourInt)
+    	checkDevice(temp, "temperature", daysInt)
+    }
+    
+    batteries.each { battery ->
+    	checkDevice(battery, "battery", daysInt)
     }
     
     contacts.each { contact ->
-    	checkDevice(contact, "contact", hourInt)
+    	checkDevice(contact, "contact", daysInt)
     }
 }
 
-private checkDevice(device, checkState, hourInt)
+private checkDevice(device, checkState, daysInt)
 {
     def state  = device.currentState(checkState)
     def time = state.date.time
     def elapsedTime = now() - time
-    def thresholdTime = hourInt * 60 * 60 * 1000 // ms = hours * 60 (min/hours) * 60 (sec/min) * 1000 (ms/sec) 
+    def thresholdTime = daysInt * 24 * 60 * 60 * 1000 // ms = days 24 (hours/day) * 60 (min/hours) * 60 (sec/min) * 1000 (ms/sec) 
 
-    log.debug "Device Status Check - Device: $device , Elapsed Time: $elapsedTime , Threshold: $thresholdTime"
+    log.debug "$checkState Device Status Check - Device: $device , Elapsed Time: $elapsedTime , Threshold: $thresholdTime"
 
     if (elapsedTime >= thresholdTime) {
         // Device has stopped responding
-        def message = "$device Stopped Responding, Reset Device"
+        def message = "$device ($checkState) Stopped Responding, Reset Device"
         send(message)
     }
 }
